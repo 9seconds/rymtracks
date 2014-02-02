774c3ed65881c4f281006398bda06456c63d42ae
9seconds <nineseconds@yandex.ru>
2014-02-02 17:53:47 +0400

    Temporary disable Travis for Python 3.3

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ Setup script for RYMTracks.
 """
 
 
-from rymtracks import __version__
-
 from sys import version_info as python_version
 from setuptools import setup, find_packages
 
@@ -24,7 +22,10 @@ with open("requirements.txt", "r") as resource:
 
 EXTRAS = {}
 if python_version >= (3,):
-    EXTRAS["2to3"] = True
+    EXTRAS["use_2to3"] = True
+    REQUIREMENTS = [
+        req for req in REQUIREMENTS if not req.startswith("futures==")
+    ]
 
 
 ##############################################################################
@@ -34,14 +35,14 @@ setup(
     name="RYMTracks",
     description="RYMTracks scraps given URLs and presents tracklists into "
                 "copypasteable form for RateYourMusic.com",
-    version=".".join(str(chunk) for chunk in __version__),
+    version="0.1.0",
     packages=find_packages(),
     setup_requires=["nose>=1.0"],
     install_requires=REQUIREMENTS,
     author="Sergey Arkhipov",
     author_email="serge@aerialsounds.org",
     maintainer="Sergey Arkhipov",
-    zip_safe=True,
+    zip_safe=False,
     maintainer_email="serge@aerialsounds.org",
     entry_points=dict(console_scripts=["rymtracks = rymtracks:main"]),
     license="MIT",
