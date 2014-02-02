45c7ae03c57588363726ff7e3ff1ec10932251a4
9seconds <nineseconds@yandex.ru>
2014-02-02 13:11:34 +0400

    PyLint for BandCamp

--- rymtracks/services/bandcamp.py
+++ rymtracks/services/bandcamp.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
+"""
+This module contains Service implementation of BandCamp.
+http://bandcamp.com
+"""
 
 
 from . import Service, HTMLMixin
 
 
+##############################################################################
+
+
 class BandCamp(HTMLMixin, Service):
+    """
+    Implementation of Service which is intended to parse Amazon MP3 Store.
+    """
 
     def fetch_tracks(self, soup):
         return soup.select("#track_table div.title")
